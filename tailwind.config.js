be875b783b279ec5e06b13283f880eb56114c5c1
Yab1 <yabilisanu@gmail.com>
2023-09-25 21:33:24 +0300

    Extended Colors and Font Families Configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,18 @@
 export default {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "intense-red": "#E61428",
+        "dark-gray": "#333333",
+        "light-gray": "#DDDDDD",
+      },
+      fontFamily: {
+        "gothic-a1": ["Gothic A1", "sans-serif"],
+        "jetBrains-mono": ["JetBrains Mono", "monospace"],
+        "noto-serif-jp": ["Noto Serif JP", "serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
