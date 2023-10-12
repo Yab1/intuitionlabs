8d98a94ac10db7f0eaaf4834b4a9b8b39075efcd
Yab1 <yabilisanu@gmail.com>
2023-10-12 20:38:00 +0300

    Reusable styled horizontal line

---
+++ src/atom/HorizontalLine.jsx
@@ -0,0 +1,9 @@
+function HorizontalLine() {
+  return (
+    <span className="relative block after:absolute after:left-0 after:right-0 after:h-px after:p-0 after:opacity-20 after:bg-white"></span>
+  );
+}
+
+HorizontalLine.displayName = "/src/atom/HorizontalLine.jsx";
+
+export default HorizontalLine;
