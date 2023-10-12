593ee40765d61aef7a5dd63da460044dd018dbb9
Yab1 <yabilisanu@gmail.com>
2023-10-13 00:38:03 +0300

    Refactor: Renamed props

--- src/atom/HorizontalLine.jsx
+++ src/atom/HorizontalLine.jsx
@@ -1,7 +1,5 @@
-function HorizontalLine({ color }) {
-  return (
-    <span className={"w-full h-px opacity-20 block " + `bg-${color}`}></span>
-  );
+function HorizontalLine({ style }) {
+  return <span className={"w-full h-px opacity-20 block " + style}></span>;
 }
 
 HorizontalLine.displayName = "/src/atom/HorizontalLine.jsx";
