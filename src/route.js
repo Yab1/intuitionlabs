96212e4b6339c9c4d08b666ee13080a280a5a247
Yab1 <yabilisanu@gmail.com>
2023-10-12 20:34:18 +0300

    Routes defined

---
+++ src/route.js
@@ -0,0 +1,24 @@
+// import { Blog, AboutUs, Contacts, Investors } from "@/pages";
+
+export const primaryRoutes = [
+  {
+    name: "Blog",
+    path: "/blog",
+    // element: <Blog />,
+  },
+  {
+    name: "About Us",
+    path: "/aboutUs",
+    // element: <AboutUs />,
+  },
+  {
+    name: "Contacts",
+    path: "/contacts",
+    // element: <Contacts />,
+  },
+  {
+    name: "Investors",
+    path: "/investors",
+    // element: <Investors />,
+  },
+];
