c4f937fcc5ff08768b2de4bada8bb8d49b238416
Yab1 <yabilisanu@gmail.com>
2023-10-12 21:09:11 +0300

    Fix: Spelling error

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 export { default as HomePage } from "@/pages/HomePage";
 export { default as Blog } from "@/pages/Blog";
 export { default as AboutUs } from "@/pages/AboutUs";
-export { default as Contact } from "@/pages/Contact";
+export { default as Contacts } from "@/pages/Contacts";
 export { default as Investors } from "@/pages/Investors";
