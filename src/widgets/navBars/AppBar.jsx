3d202779c3caa01f38aca42443f724dca8f5e7bf
Yab1 <yabilisanu@gmail.com>
2023-10-12 20:51:37 +0300

    Setup: Created boilerplate.

---
+++ src/widgets/navBars/AppBar.jsx
@@ -0,0 +1,10 @@
+import { Fragment } from "react";
+import { HomePage } from "@/pages";
+
+function AppBar() {
+  return <Fragment></Fragment>;
+}
+
+AppBar.displayName = "/src/widgets/navBars/AppBar.jsx";
+
+export default AppBar;
