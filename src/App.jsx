41f2d93738bfd6b098b0bc787ca8e2058b6e677a
Yab1 <yabilisanu@gmail.com>
2023-10-12 20:37:01 +0300

    Refactor: Imported and rendered the header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import { Fragment } from "react";
+import { Routes } from "react-router-dom";
+import { Header } from "@/layouts";
+
 function App() {
-  return <></>;
+  return (
+    <Fragment>
+      <Header />
+    </Fragment>
+  );
 }
 
 export default App;
