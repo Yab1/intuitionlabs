b20f012c1f7a16eea43848754b37dc77c1745504
Yab1 <yabilisanu@gmail.com>
2023-10-12 23:09:03 +0300

    Refactor: Removed Pseudo classes

--- src/atom/HorizontalLine.jsx
+++ src/atom/HorizontalLine.jsx
@@ -1,12 +1,6 @@
 function HorizontalLine({ color }) {
   return (
-    <span
-      // className={
-      //   "relative block after:absolute after:left-0 after:right-0 after:h-px after:p-0 after:opacity-20 " +
-      //   `after:bg-${color}`
-      // }
-      className={"w-full h-px opacity-20 block " + `bg-${color}`}
-    ></span>
+    <span className={"w-full h-px opacity-20 block " + `bg-${color}`}></span>
   );
 }
 
