cf70ea09bf87b331a96692e1ed3beb70f03d432f
Yab1 <yabilisanu@gmail.com>
2023-10-12 20:51:22 +0300

    Refactor: Imported and rendered the NavBar

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import { NavBar } from "@/widgets";
+
+function HomePage() {
+  return (
+    <section>
+      <header>
+        <NavBar />
+      </header>
+    </section>
+  );
+}
+
+HomePage.displayName = "/src/pages/HomePage.jsx";
+
+export default HomePage;
