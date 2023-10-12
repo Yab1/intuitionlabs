e1613e504fdc126d67d460413ef0d5a43a9c7bf2
Yab1 <yabilisanu@gmail.com>
2023-10-12 23:07:18 +0300

    Added Props to customize color

--- src/atom/HorizontalLine.jsx
+++ src/atom/HorizontalLine.jsx
@@ -1,6 +1,12 @@
-function HorizontalLine() {
+function HorizontalLine({ color }) {
   return (
-    <span className="relative block after:absolute after:left-0 after:right-0 after:h-px after:p-0 after:opacity-20 after:bg-white"></span>
+    <span
+      // className={
+      //   "relative block after:absolute after:left-0 after:right-0 after:h-px after:p-0 after:opacity-20 " +
+      //   `after:bg-${color}`
+      // }
+      className={"w-full h-px opacity-20 block " + `bg-${color}`}
+    ></span>
   );
 }
 
