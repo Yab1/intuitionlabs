625549f1d4ee8b67bc15cf43335c82e30f53936d
Yab1 <yabilisanu@gmail.com>
2023-10-13 21:14:03 +0300

    Refactor: set dark-gray as default color

--- src/atom/HorizontalLine.jsx
+++ src/atom/HorizontalLine.jsx
@@ -1,5 +1,9 @@
 function HorizontalLine({ style }) {
-  return <span className={"w-full h-px opacity-20 block " + style}></span>;
+  return (
+    <span
+      className={"w-full h-px bg-dark-gray opacity-20 block " + style}
+    ></span>
+  );
 }
 
 HorizontalLine.displayName = "/src/atom/HorizontalLine.jsx";
