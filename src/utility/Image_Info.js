d477c18c8155479f540dbc88892462fd76c0c056
Yab1 <yabilisanu@gmail.com>
2023-10-13 21:15:32 +0300

    Updated Data

--- src/utility/Image_Info.js
+++ src/utility/Image_Info.js
@@ -2,6 +2,8 @@ import HeroCover from "@/assets/home_page/alex-perez-pEgsWN0kwbQ-unsplash 1.svg"
 import BeatPortLink from "@/assets/home_page/beatport-link.svg";
 import Shimadzu from "@/assets/home_page/shimadzu.svg";
 import AWS from "@/assets/home_page/aws.svg";
+import AdrienLaurent from "@/assets/home_page/adrien-laurent.svg";
+import KennySchumacher from "@/assets/home_page/kenny-schumacher.svg";
 
 const Image_Info = {
   HomePage: {
@@ -26,6 +28,16 @@ const Image_Info = {
         alt: "AWS Service Logo",
       },
     ],
+    expertise: {
+      Adrien: {
+        src: AdrienLaurent,
+        alt: "Photograph of Adrien Laurent, a Co-Founder of our company.",
+      },
+      Kenny: {
+        src: KennySchumacher,
+        alt: "Photograph of Kenny Schumacher, our Head of Process, leading our team to excellence.",
+      },
+    },
   },
 };
 
