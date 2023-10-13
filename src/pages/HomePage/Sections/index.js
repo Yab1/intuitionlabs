97a2f547f15de6991824addb6970025835920629
Yab1 <yabilisanu@gmail.com>
2023-10-13 21:16:00 +0300

    Expertise Section Added

--- src/pages/HomePage/Sections/index.js
+++ src/pages/HomePage/Sections/index.js
@@ -1,2 +1,3 @@
 export { default as Hero } from "@/pages/HomePage/Sections/Hero";
 export { default as Offerings } from "@/pages/HomePage/Sections/Offerings";
+export { default as Expertise } from "@/pages/HomePage/Sections/Expertise";
